5ab4aa03253895ebd33602e1df07624d5cc584c1
Kaztro <37382584+kaztro@users.noreply.github.com>
2018-04-24 12:45:52 -0600

    Add files via upload

---
+++ VuestrosCarnes/src/vuestroscarnes/VuestrosCarnes.java
@@ -0,0 +1,19 @@
+package vuestroscarnes;
+
+/**
+ *
+ * @author kaztro
+ */
+public class VuestrosCarnes {
+
+    public static void main(String[] args) {
+        /*
+        00125717 Elsy :v
+        00139917 Kevin
+        00092217 Erick
+        Ya que lo tengan clonado le hacen algun cambio que se vea :v y le dan en commit y luego push y avisan para que los demas le demos pull.
+        */
+        
+    }
+    
+}
